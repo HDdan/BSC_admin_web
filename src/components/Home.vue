c147ce75edd72607d815c675d84ee8d962b9c04a
huangyue <huangyue@tezign.com>
2021-03-03 16:27:43 +0800

    add home

---
+++ src/components/Home.vue
@@ -0,0 +1,110 @@
+<template>
+  <el-container style="height: 500px; border: 1px solid #eee">
+    <el-header style="text-align: right; font-size: 12px">
+        <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+  <el-radio-button :label="false">展开</el-radio-button>
+  <el-radio-button :label="true">收起</el-radio-button>
+</el-radio-group>
+      <el-dropdown>
+        <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>查看</el-dropdown-item>
+          <el-dropdown-item>新增</el-dropdown-item>
+          <el-dropdown-item>删除</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <span>王小虎</span>
+    </el-header>
+
+    <el-container>
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)" v-if="isCollapse">
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+        >
+          <el-menu-item index="1">
+            <i class="el-icon-menu"></i>
+            <span slot="title">潜在经销商</span>
+          </el-menu-item>
+          <el-menu-item index="2">
+            <i class="el-icon-setting"></i>
+            <span slot="title">经销商档案</span>
+          </el-menu-item>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>基础主数据</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">修改记录</span>
+          </el-menu-item>
+          <el-menu-item index="5">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导入记录</span>
+          </el-menu-item>
+          <el-menu-item index="6">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导出记录</span>
+          </el-menu-item>
+          <el-menu-item index="7">
+            <i class="el-icon-setting"></i>
+            <span slot="title">权限设置</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <el-table :data="tableData">
+          <el-table-column prop="date" label="日期" width="140">
+          </el-table-column>
+          <el-table-column prop="name" label="姓名" width="120">
+          </el-table-column>
+          <el-table-column prop="address" label="地址"> </el-table-column>
+        </el-table>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script>
+export default {
+  name: "Left",
+  data() {
+      
+    const item = {
+      date: "2016-05-02",
+      name: "王小虎",
+      address: "上海市普陀区金沙江路 1518 弄",
+    };
+    return {
+        isCollapse: true,
+      tableData: Array(20).fill(item),
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
--- src/routes/login.js
+++ src/routes/login.js
@@ -1,10 +1,16 @@
 // const login   = () => import(/* webpackChunkName: "login" */ '@/views/login/index');
 import login from '@/views/login/index'
+import Home from '@/components/Home'
 export default [
   {
     path: '/login/index', 
     name: 'login',
     component: login, 
     meta: { title: '登陆' } 
+  },
+  {
+    path: '/home', 
+    name: 'Home',
+    component: Home, 
   },
 ];
